3ec79763e9a8e5a654945043b9b749f87915f625
Grechka <kurchenko-e@mail.ru>
2021-02-15 00:51:41 +0300

    Лабораторная работа №1/ Задание №2

--- #1/#1.pl
+++ #1/#1.pl
@@ -30,3 +30,37 @@ woman(valeria).
 
 men(X):-man(X), write(X), write(" "), fail.
 women(X):-woman(X), write(X), write(" "), fail.
+
+parent(pavel,ivan).
+parent(anna,ivan).
+
+parent(alexey,liza).
+parent(alexey,sveta).
+parent(alexey,maksim).
+parent(alexey,dasha).
+parent(lydmila,liza).
+parent(lydmila,sveta).
+parent(lydmila,maksim).
+parent(lydmila,dasha).
+
+parent(katya,fedor).
+parent(katya,sasha).
+parent(zhenia,fedor).
+parent(zhenia,sasha).
+
+parent(ivan,irina).
+parent(ivan,anfisa).
+parent(ivan,valeria).
+parent(liza,irina).
+parent(liza,anfisa).
+parent(liza,valeria).
+
+parent(dasha,egor).
+parent(dasha,kirill).
+parent(dasha,kolya).
+parent(fedor,egor).
+parent(fedor,kirill).
+parent(fedor,kolya).
+
+child(X,Y):-parent(Y,X).
+children(X):-child(Y,X), write(Y), write(" "), fail.
