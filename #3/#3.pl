d27d804cc7cf10b545520de2bacf9ef7e0d84287
Grechka <kurchenko-e@mail.ru>
2021-02-28 17:35:04 +0300

    Лабораторная работа №3 / Задание №5

--- #3/#3.pl
+++ #3/#3.pl
@@ -14,3 +14,9 @@ fact(N,X):- N1 is N-1, fact(N1,X1), X is X1*N.
 fact(1, CurX, CurX):-!.
 fact(N,CurX,X):-CurX1 is CurX*N, N1 is N-1, fact(N1, CurX1, X).
 factorial(N,X):-fact(N,1,X). 
+
+%рекурсия вверх
+fib(0,0):-!.
+fib(1,1):-!.
+fib(2,1):-!.
+fib(N,X):- N1 is N-1, N2 is N-2, fib(N1,X1), fib(N2,X2), X is X1+X2.
