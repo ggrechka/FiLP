d3f7990580001218e24d8f7c4fd792b99c889045
Grechka <kurchenko-e@mail.ru>
2021-02-28 17:27:54 +0300

    Лабораторная работа №3 / Задание №4

--- #3/#3.pl
+++ #3/#3.pl
@@ -8,3 +8,9 @@ max(_,_,U,U).
 %рекурсия вверх
 fact(1,1):-!.
 fact(N,X):- N1 is N-1, fact(N1,X1), X is X1*N.
+
+
+%рекурсия вниз
+fact(1, CurX, CurX):-!.
+fact(N,CurX,X):-CurX1 is CurX*N, N1 is N-1, fact(N1, CurX1, X).
+factorial(N,X):-fact(N,1,X). 
