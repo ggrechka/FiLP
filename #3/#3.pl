32ab2cb780df72a5bb5f27d597660d540aa26a9e
Grechka <kurchenko-e@mail.ru>
2021-02-28 22:07:47 +0300

    Лабораторная работа №3 / Задание №14-1

--- #3/#3.pl
+++ #3/#3.pl
@@ -1,19 +1,28 @@
+%1 номер
+
 max(X,Y,X):- X>=Y.
 max(_,Y,Y).
+
+%2 номер
 
 max(X, Y, U, X):-X>=Y, X>=U.
 max(_,Y, U, Y):- Y>=U.
 max(_,_,U,U).
 
+%3 номер
+
 %рекурсия вверх
 fact(1,1):-!.
 fact(N,X):- N1 is N-1, fact(N1,X1), X is X1*N.
 
+%4 номер
 
-%рекурсия вниз
+%рекурсия вниз - доп переменные
 fact(1, CurX, CurX):-!.
 fact(N,CurX,X):-CurX1 is CurX*N, N1 is N-1, fact(N1, CurX1, X).
 factorial(N,X):-fact(N,1,X). 
+
+%5 номер
 
 %рекурсия вверх
 fib(0,0):-!.
@@ -21,27 +30,39 @@ fib(1,1):-!.
 fib(2,1):-!.
 fib(N,X):- N1 is N-1, N2 is N-2, fib(N1,X1), fib(N2,X2), X is X1+X2.
 
+%6 номер
+
 %рекурсия вниз
 fib(0,A,_,A):-!.
 fib(N,A,B,X):- AB is A+B, N1 is N-1, fib(N1,B,AB,X).
 fibonachi(N,X):-fib(N,0,1,X).
 
+%7 номер
+
 sumnumbers(N,N):- N<10.
 sumnumbers(N,X):- N>=10, D is N div 10, M is N mod 10, sumnumbers(D,X1), X is M+X1. 
+
+%8 номер
 
 sum(0,Sum,Sum):-!.
 sum(N,Sum,X):- D is N div 10, M1 is N mod 10, Sum1 is Sum + M1,sum(D,Sum1,X). 
 sumnumbers1(N,X):-sum(N,0,X).
+
+%9 номер
 
 %рекурсия вверх
 maxDigit(N,N):- N<10.
 maxDigit(N,X):- N1 is N div 10, N2 is N mod 10, maxDigit(N1,X1), N2=<X1,!, X is X1.
 maxDigit(N,X):- N1 is N div 10, N2 is N mod 10, maxDigit(N1,X1), N2>X1, X is N2.
 
+%10 номер
+
 %рекурсия вниз
 mDig(0,A,A):- !.
 mDig(A,B,C):- D is A mod 10, D>B, !, E is A div 10, mDig(E,D,C).
 mDig(A,B,C):- D is A div 10, mDig(D,B,C).
+
+%11 номер
 
 %рекурсия вверх
 min(N,N):-N<10.
@@ -54,6 +75,7 @@ min1(N,B,X):- D is N mod 10, 1 is D mod 2, D<B, !, E is N div 10, min1(E,D,X).
 min1(N,B,X):- D is N div 10, min1(D,B,X).
 minimal(N,X):-min1(N,10,X).
 
+%12 номер
 nod(A,A,A):-!.
 nod(0,B,B):-!.
 nod(A,0,A):-!.
@@ -66,3 +88,8 @@ kol_del123(N,Count):-kol_del(N,N,Count).
 
 simple(1):-true.
 simple(X):- kol_del123(X,C), 2 is C. 
+
+%14 номер
+kol_del_3(1,_,1):-!.
+kol_del_3(I,N,Count):-I1 is I-1, kol_del_3(I1,N,C), ((0 is N mod I), not(0 is I mod 3) -> Count is C+1;Count is C).
+kol_del_333(N,Count):-kol_del_3(N,N,Count).
