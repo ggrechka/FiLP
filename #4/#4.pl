3c46e5f0770110c1e00d5c4444516408795ab169
Grechka <kurchenko-e@mail.ru>
2021-03-01 08:28:15 +0300

    Лабораторная работа №4 / Задание №1

---
+++ #4/#4.pl
@@ -0,0 +1,10 @@
+%1 задание
+append_element([], X, X).
+append_element([H|T1], X, [H|T2]) :- append_element(T1, X, T2).
+
+read_list(N, L) :- read_list(N, 0, [], L), !.
+read_list(N, N, L, L) :- !.
+read_list(N, CurN, CurL, L) :- CurN1 is CurN + 1, read(X), append_element(CurL, [X], CurL1), read_list(N, CurN1, CurL1, L).
+
+write_list([]) :- !.
+write_list([H|T]) :- write(H), write(" "), write_list(T).
